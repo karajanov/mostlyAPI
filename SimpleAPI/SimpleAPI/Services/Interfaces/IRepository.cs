0f5fa9b09bf25c20175819feac8e38cb86689d2d
karajanov <karajanovb@yahoo.com>
2020-02-07 03:42:39 +0100

    base established

---
+++ SimpleAPI/SimpleAPI/Services/Interfaces/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimpleAPI.Services.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(object id);
+        Task InsertAsync(T obj);
+        Task UpdateAsync(T obj);
+        Task DeleteAsync(object id);
+        Task SaveAsync();
+    }
+}
